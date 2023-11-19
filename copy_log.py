f552cef5a6c1d8feb45146cfda3b48a825d4c42b
7shi <7shi@live.jp>
2023-11-19 22:01:01 +0900

    update script

--- copy_log.py
+++ copy_log.py
@@ -1,26 +1,33 @@
-import sys, os, io, pyperclip
+import sys, os, re, io, pyperclip
 
 if len(sys.argv) != 2:
     print(f"Usage: python {sys.argv[0]} file")
     sys.exit(1)
 
+def waitForNewPaste():
+    ret = pyperclip.waitForNewPaste().replace("\r\n", "\n").split("\n")
+    while ret and not ret[-1]:
+        ret.pop()
+    return ret
+
 file = sys.argv[1]
 while True:
     print("prompt?")
-    p = pyperclip.waitForNewPaste().replace("\r\n", "\n").split("\n")
-    while p and not p[-1]:
-        p.pop()
+    p = waitForNewPaste()
     print("response?")
-    r = pyperclip.waitForNewPaste().replace("\r\n", "\n")
-    r.removesuffix("\n")
+    r = waitForNewPaste()
     with io.StringIO() as sio:
         if os.path.exists(file):
             print(file=sio)
-            print("----", file=sio)
-            print(file=sio)
+        print("##", file=sio)
+        print(file=sio)
         for line in p:
             print("    " + line, file=sio)
         print(file=sio)
-        print(r, file=sio)
+        for line in r:
+            if m := re.match(r"#+(.*)", line):
+                print("**", m.group(1).strip(), "**" ,sep="", file=sio)
+            else:
+                print(line, file=sio)
         with open(file, "ab") as f:
             f.write(sio.getvalue().encode("utf_8"))
